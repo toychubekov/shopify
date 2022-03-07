f82dfe6030dfb85f5b60b7b4e82293cf00df2ab8
toychubekov <toychubekovbekzat5@gmail.com>
2022-03-07 09:03:25 +0300

    'init'

---
+++ src/js/script.js
@@ -0,0 +1,73 @@
+$('.mobile-btn').on('click', function () {
+    $('.mobile-btn').toggleClass('show-mobile-btn');
+    $('.header-menu').toggleClass('show-menu');
+    $('.header-link').toggleClass('show-header-link');
+    $('.header-link1').toggleClass('show-header-link');
+
+});
+$('.header_menu a').on('click', function () {
+    $('.mobile-btn').removeClass('show-mobile-btn');
+    $('.header-menu').removeClass('show-menu');
+    $('.header-link').removeClass('show-menu');
+    $('.header-link1').removeClass('show-menu');
+});
+
+
+$(document).ready(function () {
+    $(".carousel").owlCarousel({
+        items: 4,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+    });
+});
+
+
+let popupBg = document.querySelector('.popup-bg');
+let popup = document.querySelector('.popup');
+let openPopupButtons = document.querySelectorAll('.header-btn',);
+let closePopupButton = document.querySelector('.close-popup');
+
+
+
+openPopupButtons.forEach((button) =>{
+    button.addEventListener('click',(e )=>{
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+});
+
+closePopupButton.addEventListener('click', () => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+document.addEventListener('click', (e )  => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
